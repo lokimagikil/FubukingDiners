aae4c20117e3e239a2e26d6f685fb6bdf93459d0
jinkiyoshi <64452856+jinkiyoshi@users.noreply.github.com>
2020-09-10 14:04:34 +0800

    Recommit 2

--- Build/FubukingDiners.framework.js
+++ Build/FubukingDiners.framework.js
@@ -1297,7 +1297,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2752320;
+STATICTOP = STATIC_BASE + 2752512;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3227,7 +3227,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2752320;
+var STATIC_BUMP = 2752512;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
@@ -15338,8 +15338,8 @@ function nullFunc_vjji(x) {
  err("Build with ASSERTIONS=2 for more info.");
  abort(x);
 }
-Module["wasmTableSize"] = 56641;
-Module["wasmMaxTableSize"] = 56641;
+Module["wasmTableSize"] = 56644;
+Module["wasmMaxTableSize"] = 56644;
 function invoke_dd(index, a1) {
  var sp = stackSave();
  try {
